3273e9b872d074924d096813a7eb4530efe69870
Ivo Lopes <charro.il@gmail.com>
2018-05-26 18:27:30 +0200

    add dht11 code link

--- code/dht11.py
+++ code/dht11.py
@@ -4,12 +4,12 @@ import Adafruit_DHT
 #Pick the sensor type, DHT11, DHT22 or AM2302 
 sensor=Adafruit_DHT.DHT11
 
-def TempHum_Sensor(channel):
-	#Using the read_retry method in order to get at least 1 good measure, and waiting 2 seconds between each retry
-	humidity, temperature = Adafruit_DHT.read_retry(sensor,channel)
-
-	#The sensor occasionally fails to return good data and we can only make a read every 2 seconds
-	if humidity is not None and temperature is not None:
-		print("Temp={0:0.1f}°C Humidity={1:0.1f}%".format(temperature, humidity))
-	else:
-		print("Failed to read the data, please try again!")
+def tempHum_Sensor(channel):
+    print ("canal {} e sensor {}".format(channel, sensor))
+    #Using the read_retry method in order to get at least 1 good measure, and waiting 2 seconds between each retry
+    humidity, temperature = Adafruit_DHT.read_retry(sensor,channel)
+    #The sensor occasionally fails to return good data and we can only make a read every 2 seconds
+    if humidity is not None and temperature is not None:
+        print("Temp={0:0.1f}°C Humidity={1:0.1f}%".format(temperature, humidity))
+    else:
+        print("Failed to read the data, please try again!")
--- code/main.py
+++ code/main.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python
 import sys
 import sensor_test
+import dht11
 
 rpi_input = 0
 
@@ -8,6 +9,7 @@ rpi_input = 0
 def menu():
     print("""Choose an action: 
 		R: Run the sensor test
+		T: Run the DHT11 temperature and humidity sensor
 		Q: Quit the program
 		""")
     while True:
@@ -18,21 +20,38 @@ def menu():
         if action_input in ["q", "Q"]:
             print("Exiting the program")
             sys.exit(1)
+        
+        if action_input in ["t","T"]:
+            action_T()
 
 
 #get user input
 def get_command():
     return input("Action: ")
 
-#when the user press R, demands for the channel and verify if it is a number
-def action_R():
+#get channel in the GPIO.BOARD format
+def get_channel():
     print("In what channel (GPIO.BOARD format) the sensor data pin is connected?")
-    rpi_input = input("Channel: ")
+    return input("Channel: ")
+
+#when the user press R or T, demands for the channel and verify if it is a number
+def action_R():
+    rpi_input = get_channel()
     if rpi_input.isdigit():
         sensor_test.testing(rpi_input)
     else:
         print("""You must enter a number
         """)
         action_R()
+        
+def action_T():
+    print("In what channel (GPIO.BCM format) the sensor data pin is connected?")
+    rpi_input = input("Channel: ")
+    if rpi_input.isdigit():
+        dht11.tempHum_Sensor(rpi_input)
+    else:
+        print("""You must enter a number
+        """)
+        action_T()
 
 menu()
